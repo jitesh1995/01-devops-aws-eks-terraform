06e64461c6bce9f2a198c96e3e6b7a5a453ebd99
Jitesh Khemchandani <jxk7806@mavs.uta.edu>
2025-09-29 16:18:32 -0500

    chore(terraform): fmt + add .terraform.lock.hcl

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "demo-vpc" }
+  tags                 = { Name = "demo-vpc" }
 }
 
 resource "aws_subnet" "public" {
@@ -10,5 +10,5 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
-  tags = { Name = "demo-public-${count.index}" }
+  tags                    = { Name = "demo-public-${count.index}" }
 }
